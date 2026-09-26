4fc3d4f85a77236828efc502a76012250bb8674c
kevin781130-netizen <kevin781130@gmail.com>
2026-09-26 23:54:43 +0800

    Include shared-region map in DNNI golden path

--- PREPARE_DNNI_GOLDEN_PATH.bat
+++ PREPARE_DNNI_GOLDEN_PATH.bat
@@ -13,5 +13,8 @@ if "%~1"=="" (
 "%PY%" "runtime\dnni_dtype_map_probe.py" "%MODEL_DIR%" --out "%MODEL_DIR%\dnni_dtype_map.json"
 if errorlevel 1 exit /b %ERRORLEVEL%
 
+"%PY%" "runtime\dnni_shared_region_probe.py" "%MODEL_DIR%" --out "%MODEL_DIR%\dnni_shared_regions.json"
+if errorlevel 1 exit /b %ERRORLEVEL%
+
 "%PY%" "runtime\dnni_golden_path_probe.py" "%MODEL_DIR%" --dtype-map "%MODEL_DIR%\dnni_dtype_map.json" --out "%MODEL_DIR%\violin_a4_golden_path.json"
 exit /b %ERRORLEVEL%
